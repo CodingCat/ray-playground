182f68da9233345df4e1ccac2e2013328ac339ad
CodingCat <zhunansjtu@gmail.com>
2025-12-18 22:51:52 -0800

    refactor and data fetching example

---
+++ src/ray_core/parallel_database_fetching.py
@@ -0,0 +1,35 @@
+import time
+import os
+import ray
+from ray.runtime_context import get_runtime_context
+
+database = [
+    "Learning", "Ray",
+    "Flexible", "Distributed", "Python", "for", "Machine", "Learning"
+]
+
+
+def retrieve(item):
+    time.sleep(item / 10.)
+    return item, database[item]
+
+def print_runtime(input_data, start_time):
+    print(f'Runtime: {time.time() - start_time:.2f} seconds, data:')
+    print(*input_data, sep="\n")
+
+
+start = time.time()
+data = [retrieve(item) for item in range(8)]
+print_runtime(data, start)
+
+@ray.remote
+def retrieve_task(item):
+    ctx = get_runtime_context()
+    print(f"running item={item}, task_id={ctx.get_task_id()}")
+    return retrieve(item)
+
+start = time.time()
+object_references = [retrieve_task.remote(item) for item in range(8)]
+
+data = ray.get(object_references)
+print_runtime(data, start)
